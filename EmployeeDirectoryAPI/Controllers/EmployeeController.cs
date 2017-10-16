0426a4e9b2e2826486b71120c92c01d01a9a2843
awais.dilber@hotmail.com <Pakistanig46>
2017-10-16 21:34:40 +0500

    Employee Directory API

---
+++ EmployeeDirectoryAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,82 @@
+﻿using EmployeeDirectoryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeDirectoryAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        IList<Employee> employees = new List<Employee>()  
+        {  
+            new Employee()  
+                {  
+                    EmployeeId = "1", 
+                    EmployeeName = "Amin Ansari", 
+                    Department = "Management",
+                    Ext = "101",
+                    ContactNumber = "0312-842-9631",
+                    EmergencyContactNumber = "0312-842-9631",
+                    Address = "Model Town" 
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = "2", 
+                    EmployeeName = "Nadeem Ahmed", 
+                    Department = "Application Development",
+                    Ext = "104",
+                    ContactNumber = "0300-842-9631",
+                    EmergencyContactNumber = "0300-842-9631",
+                    Address = "Bahria Town" 
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = "4", 
+                    EmployeeName = "Shahid Ashraf", 
+                    Department = "Application Support",
+                    Ext = "129",
+                    ContactNumber = "0321-842-9631",
+                    EmergencyContactNumber = "0321-842-9631",
+                    Address = "Bahria Town" 
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = "3", 
+                    EmployeeName = "Fawad Zia Qureshi", 
+                    Department = "Quality Assurance",
+                    Ext = "103",
+                    ContactNumber = "0345-842-9631",
+                    EmergencyContactNumber = "0345-842-9631",
+                    Address = "BR Society" 
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = "5", 
+                    EmployeeName = "Muhammad Reza", 
+                    Department = "Software Development",
+                    Ext = "129",
+                    ContactNumber = "0333-842-9631",
+                    EmergencyContactNumber = "0333-842-9631",
+                    Address = "Model Town Extension" 
+                }  
+        };
+        public IList<Employee> GetAllEmployees()
+        {
+            //Return list of all employees  
+            return employees;
+        }
+        public Employee GetEmployeeDetails(string id)
+        {
+            //Return a single employee detail  
+            var employee = employees.FirstOrDefault(e => e.EmployeeId.Equals(id));
+            if (employee == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return employee;
+        }
+    }
+}
---
+++ EmployeeDirectoryAPI/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDirectoryAPI.Models
+{
+    public class Employee
+    {
+        public string EmployeeId
+        {
+            get;
+            set;
+        }
+        public string EmployeeName
+        {
+            get;
+            set;
+        }
+        public string Department
+        {
+            get;
+            set;
+        }
+        public string Ext
+        {
+            get;
+            set;
+        }
+        public string ContactNumber
+        {
+            get;
+            set;
+        }
+        public string EmergencyContactNumber
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+    }
+}
